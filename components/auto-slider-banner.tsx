b83352bfbe4883d35ac35151e0ad3a5a2a56dad2
John Carroll <13478618+john-carroll-sw@users.noreply.github.com>
2025-06-02 15:26:58 -0400

    feat: implement auto slider banner with video support; add multiple video files for enhanced user experience

--- components/auto-slider-banner.tsx
+++ components/auto-slider-banner.tsx
@@ -6,9 +6,13 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import { Github, Linkedin, Mail } from "lucide-react"
 
-const images = [
-	"https://64.media.tumblr.com/db8472cfbb89a155148003b053d5f3de/4d6d987e0cee7307-8e/s400x225/158142e8e876044a6191733a02f6ee5ac1643b58.gif",
-	"https://i.pinimg.com/originals/14/f4/35/14f435eaaf8d107cca5055ce150eaf47.gif",
+const videos = [
+	"/videos/2675508-hd_1920_1080_24fps.mp4",
+	"/videos/3129671-hd_1920_1080_30fps.mp4",
+	"/videos/3141210-hd_1920_1080_25fps.mp4",
+	"/videos/3163534-hd_1920_1080_30fps.mp4",
+	"/videos/4990242-hd_1920_1080_30fps.mp4",
+	"/videos/4990245-hd_1920_1080_30fps.mp4",
 ]
 
 export function AutoSliderBanner() {
@@ -16,29 +20,28 @@ export function AutoSliderBanner() {
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
-		}, 5000) // Change image every 5 seconds
-
+			setCurrentIndex((prevIndex) => (prevIndex + 1) % videos.length)
+		}, 10000) // Change video every 5 seconds
 		return () => clearInterval(interval)
 	}, [])
 
 	return (
 		<div className="relative top-0 left-0 w-screen h-screen z-0 overflow-hidden">
-			{images.map((src, index) => (
-				<div
+			{videos.map((src, index) => (
+				<video
 					key={src}
-					className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ${
+					className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ${
 						index === currentIndex ? "opacity-100" : "opacity-0"
 					}`}
+					autoPlay
+					loop
+					muted
+					playsInline
+					poster="/placeholder.jpg"
 				>
-					<Image
-						src={src || "/placeholder.svg"}
-						alt={`Banner ${index + 1}`}
-						fill
-						style={{ objectFit: "cover" }}
-						priority
-					/>
-				</div>
+					<source src={src} type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
 			))}
 			<div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
 				<h1 className="text-5xl md:text-7xl font-extrabold tracking-tight mb-6 text-gray-100">
